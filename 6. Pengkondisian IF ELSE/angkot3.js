27aa007ac5005dc1115cc2ddd11ff2393cde24c6
zabiths <renvian09@gmail.com>
2023-12-20 15:10:17 +0700

    Masuk materi if else, else if dan latihan juragan angkot

---
+++ 6. Pengkondisian IF ELSE/angkot3.js
@@ -0,0 +1,11 @@
+let jumlahAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1; 
+
+for (noAngkot; noAngkot <= jumlahAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log("Angkot NO." + noAngkot + " beroperasi dengan baik");
+  } else {
+    console.log("Angkot NO." + noAngkot + " sedang tidak beroperasi");
+  }
+}
---
+++ 6. Pengkondisian IF ELSE/script.js
@@ -0,0 +1,7 @@
+let angka = prompt("masukan angka:");
+
+if (angka % 2 == 0) {
+  alert(angka + " " + "adalah bilangan GENAP");
+} else { 
+  alert(angka + " " + "adalah bilang GANJIL");
+}
