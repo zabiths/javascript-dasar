cd85c07dac5cc3229bcc1e70800fb6f132ceade3
zabiths <renvian09@gmail.com>
2023-12-27 15:06:43 +0700

    Materi function declaration dan function expression

---
+++ 15. Function Declaration dan Function Expression/script.js
@@ -0,0 +1,13 @@
+// function declaration
+// function tampilPesan(nama) {
+//   alert("halo" + nama);
+// }
+
+// tampilPesan("Rendy");
+
+// function expression
+let tampilPesan = function (name) {
+  alert("halo" + nama);
+};
+
+tampilPesan("alfian");
