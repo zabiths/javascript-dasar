6e5095f6bc140d087aab7a8c5ba0f65597c874dd
M. Rendy Alfian <67450398+zabiths@users.noreply.github.com>
2023-12-29 04:47:58 +0000

    Method pada Array

--- 16. Array/script.js
+++ 16. Array/script.js
@@ -20,8 +20,29 @@
 // console.log(arr);
 
 // 3. Menampilkan isi Array
-let arr = ["Muhammad", "Rendy", "Alfian"];
+// let arr = ["Muhammad", "Rendy", "Alfian"];
 
-for (let i = 0; i < arr.length; i++) {
-  console.log("Mahasiswa ke- " + (i + 1) + " : " + arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   console.log("Mahasiswa ke- " + (i + 1) + " : " + arr[i]);
+// }
+
+// Method pada array
+let arr = ["Muhammad", "Rendy", "Alfian"];
+// 1. join
+// console.log(arr.join(" - "));
+
+// 2. push & pop
+// push menambah elemen terakhir sebuah array
+// push juga bisa untuk menambahkan beberapa elemen sekaligus.
+// pop menghapus elemen terakhir sebuah array
+// joko akan ditambah kedalam array dengan menggunakan method push.
+// arr.push("Joko");
+// arr.pop();
+// arr.pop();
+// console.log(arr.join(" - "));
+
+// 3. unsift & shift
+// sama seperti push & pop, tapi ini menambah dan menghapus elemen pertama.
+// arr.unshift("Dodo");
+// arr.shift();
+console.log(arr.join(" - "));
